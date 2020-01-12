870456b17f4e59ea0811866e19d8822f6a88fa48
martincallesen <mail@martincallesen.dk>
2020-01-12 11:40:07 +0100

    added solution for fizzbuzz

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -1,2 +1,37 @@
 public class FizzBuzz {
+    private final Printer printer;
+
+    public FizzBuzz(Printer printer) {
+        this.printer = printer;
+    }
+
+    public void print(int number) {
+        String msg = "";
+
+        if(shouldPrintFizz(number)){
+            msg = "Fizz";
+        }
+
+        if(shouldPrintBuzz(number)){
+            msg += "Buzz";
+        }
+
+        if(shouldPrintNumber(number) && msg.equals("")){
+            msg += number;
+        }
+
+        printer.print(msg);
+    }
+
+    private boolean shouldPrintNumber(int number) {
+        return number > 0 && number < 101;
+    }
+
+    private boolean shouldPrintBuzz(int number) {
+        return number % 5 == 0 && shouldPrintNumber(number);
+    }
+
+    private boolean shouldPrintFizz(int number) {
+        return number % 3 == 0  && shouldPrintNumber(number);
+    }
 }
--- FizzBuzz/src/test/java/FizzBuzzTest.java
+++ FizzBuzz/src/test/java/FizzBuzzTest.java
@@ -1,21 +1,37 @@
 import org.junit.Ignore;
 import org.junit.Test;
 
+import static org.hamcrest.CoreMatchers.is;
+import static org.junit.Assert.assertThat;
+
 public class FizzBuzzTest {
     @Test
-    @Ignore
     public void printFizz() {
-
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Fizz"))).print(3);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Fizz"))).print(6);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Fizz"))).print(9);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Fizz"))).print(12);
     }
 
     @Test
-    @Ignore
+    public void printNumber() {
+        new FizzBuzz(msg -> assertThat("Prints", msg, is(""))).print(0);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("1"))).print(1);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is(""))).print(101);
+    }
+
+    @Test
     public void printBuzz() {
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Buzz"))).print(5);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Buzz"))).print(10);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("Buzz"))).print(20);
     }
 
     @Test
-    @Ignore
     public void printFizzBuzz() {
-
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("FizzBuzz"))).print(15);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("FizzBuzz"))).print(30);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("FizzBuzz"))).print(45);
+        new FizzBuzz(msg -> assertThat("Prints", msg, is("FizzBuzz"))).print(60);
     }
 }
