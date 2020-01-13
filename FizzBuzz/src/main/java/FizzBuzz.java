20a443681dd8ed38f91ddff9254ac065a8b8c916
Martin Callesen <mca@bec.dk>
2020-01-13 14:32:12 +0100

    added solution for fizzbuzz

--- FizzBuzz/src/main/java/FizzBuzz.java
+++ FizzBuzz/src/main/java/FizzBuzz.java
@@ -6,31 +6,27 @@ public class FizzBuzz {
     }
 
     public void print(int number) {
-        String msg = "";
-
-        if(shouldPrintFizz(number)){
-            msg = "Fizz";
+        if(shouldPrintFizzBuzz(number) ) {
+            printer.print("FizzBuzz");
+        } else if(shouldPrintBuzz(number) ){
+            printer.print("Buzz");
+        } else if(shouldPrintFizz(number)){
+            printer.print("Fizz");
+        } else if (shouldPrintNumber(number)){
+            printer.print(""+number);
         }
-
-        if(shouldPrintBuzz(number)){
-            msg += "Buzz";
-        }
-
-        if(shouldPrintNumber(number) && msg.equals("")){
-            msg += number;
-        }
-
-        printer.print(msg);
     }
 
     private boolean shouldPrintNumber(int number) {
         return number > 0 && number < 101;
     }
 
+    private boolean shouldPrintFizzBuzz(int number) {
+        return number % 15 == 0 &&  shouldPrintNumber(number);
+    }
     private boolean shouldPrintBuzz(int number) {
         return number % 5 == 0 && shouldPrintNumber(number);
     }
-
     private boolean shouldPrintFizz(int number) {
         return number % 3 == 0  && shouldPrintNumber(number);
     }
